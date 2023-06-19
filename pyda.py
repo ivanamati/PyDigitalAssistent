a997b8ba3ecf4cf8a5a6b90fdb0196b661bd4fa5
IvanaM <ivanamati@gmail.com>
2023-06-19 18:58:57 +0200

    german & english speech

--- pyda.py
+++ pyda.py
@@ -24,14 +24,14 @@ class My_Py_Asisstant_App:
         self.window.title("My Py Digital Asisstant ")   
         self.window.geometry("500x150")   
  
-        # Name Label
+        # Label
         self.greetinglabenl = ctk.CTkLabel(self.window,
                                       text="How can IPy help you?")
         self.greetinglabenl.grid(row=0, column=0,
                             padx=20, pady=20,
                             sticky="ew")
  
-        # Name Entry Field
+        # Question entry
         self.korisnikovo_pitanje = ctk.CTkEntry(self.window,
                          placeholder_text="your input here", width=300)
         self.korisnikovo_pitanje.grid(row=0, column=1,
@@ -43,18 +43,18 @@ class My_Py_Asisstant_App:
                                          text="OK", 
                                          command=self.popup_prozor,
                                          width=10)
+        
+        # CANCEL button
         self.gumb_za_rezutat.grid(row=2, column=1,                                   
                                         padx=10, pady=10,
                                         sticky="ew")
         self.gumb_cancel = ctk.CTkButton(self.window, text="Cancel", command=self.izlaz_iz_app,width=10)
         self.gumb_cancel.grid(row=2, column=2, sticky="ew", pady=20)
 
-
-
+        # combobox za odabir jezika pitanja i odgovora
         combobox_var = ctk.StringVar(value="choose the language")  # set initial value
         self.odabir_jezika_asistenta = ctk.CTkComboBox(master=self.window,
-                                     values=["en", "de", "fr"],
-                                     #command=lambda: self.combobox_callback,
+                                     values=["en", "de", "hr"],
                                      variable=combobox_var)
         self.odabir_jezika_asistenta.grid(row=1,column=1,padx=10,
                                         sticky="ew")
@@ -64,77 +64,80 @@ class My_Py_Asisstant_App:
         popup_window = ctk.CTkToplevel()
         popup_window.grab_set() 
         popup_window.title("Message")
-        popup_window.geometry("600x250")
-
-        odgovor = self.ispis_odgovora_na_korisnikov_input()
-        odabrani_jezik = self.odabir_jezika_asistenta.get()
-        print(odabrani_jezik)
-
+        popup_window.geometry("600x450")     
+        
         # voice engine for digital asisstent
+
+        # ODGOVOR:
+        odgovor = self.odgovor_na_korisnikov_input()
+
+        popup_label = ctk.CTkLabel(popup_window, text="the answer is:")
+        popup_label.pack(pady=10)
+
+        # ovdje se ispsuje odgovor:
+        popup_answer = ctk.CTkLabel(popup_window, text=odgovor, wraplength=600)
+        popup_answer.pack(pady=20)
+
+        # # this code helps that sound comes 1 second after the tekst
+        # self.window.after(1000, lambda: engine.say(odgovor))
+        # self.window.after(1000, engine.runAndWait)
+
+                # promjena jezika s obzirom na odabir korisnika
         engine = pyttsx3.init()
+        odabrani_jezik = self.odabir_jezika_asistenta.get()
+        print("vas odabrani jezik je: ", odabrani_jezik)
 
         if odabrani_jezik == "en":
             voices = engine.getProperty('voices')
             engine.setProperty('voice', voices[1].id) # ili "voices[0].id" ovisno o željenom glasu
+                    # this code helps that sound comes 1 second after the tekst
+            self.window.after(1000, lambda: engine.say(odgovor))
+            self.window.after(1000, engine.runAndWait)
 
-        elif odabrani_jezik == "de":
+        if odabrani_jezik == "de":
             german_voice_id = "com.apple.speech.synthesis.voice.anna"
             engine.setProperty('voice', german_voice_id)
+                    # this code helps that sound comes 1 second after the tekst
+            self.window.after(1000, lambda: engine.say(odgovor))
+            self.window.after(1000, engine.runAndWait)
 
-        elif odabrani_jezik == "fr":
-            voices = engine.getProperty('voices')
-            french_voice_id = "com.apple.speech.synthesis.voice.amelie"
-            engine.setProperty('voice', french_voice_id)
+        elif odabrani_jezik == "hr":
+            engine.stop()
 
-        # voices = engine.getProperty('voices')
-        # engine.setProperty('voice', voices[1].id) # ili "voices[0].id" ovisno o željenom glasu
-
-        # odgovor = self.ispis_odgovora_na_korisnikov_input()
-
-        popup_label = ctk.CTkLabel(popup_window, text="the answer is:")
-        popup_label.pack(pady=10)
-        popup_answer = ctk.CTkLabel(popup_window, text=odgovor, wraplength=500)
-        popup_answer.pack(pady=20)
-
-        # this code helps that sound comes 1 second after the tekst
-        self.window.after(1000, lambda: engine.say(odgovor))
-        self.window.after(1000, engine.runAndWait)
-
+        # GO BACK
         popup_button = ctk.CTkButton(popup_window, text="GO BACK",
                                      command=popup_window.destroy)
         popup_button.place(anchor="center",relx=0.5, rely=0.8)
 
 
-    def ispis_odgovora_na_korisnikov_input(self):
+    def odgovor_na_korisnikov_input(self):
         client = wolframalpha.Client("ATVE7L-94QT4PX37R")
 
         # user input
         upit = self.korisnikovo_pitanje.get()
         odabrani_jezik = self.odabir_jezika_asistenta.get()
-
-        # voice engine for digital asisstent
-        engine = pyttsx3.init()
-        voices = engine.getProperty('voices')
-        engine.setProperty('voice', voices[1].id)
+        print("sad cu govoriti na: ", odabrani_jezik, "jeziku.")
 
         try:
-            jezik = wikipedia.set_lang(odabrani_jezik)
+            wikipedia.set_lang(odabrani_jezik)
             wiki_rezultat = wikipedia.summary(upit, sentences=2)
             return wiki_rezultat
 
         except wikipedia.exceptions.DisambiguationError:
-            
             wolfram_res = next(client.query(upit).results).text
-            print("odgovor je bio ovdje")
+            print("tražio sam na wolframu")
             return wolfram_res
 
         except wikipedia.exceptions.PageError:
-            
             wolfram_res = next(client.query(upit).results).text
-            print("ovdje sam sada")
+            print("i sada sam tražio na wolframu")
             return wolfram_res
         
         except:
+            # voice engine for digital asisstent
+            engine = pyttsx3.init()
+            voices = engine.getProperty('voices')
+            engine.setProperty('voice', voices[1].id)
             engine.say("I'm sorry, I can't find the answer to your question")
             engine.runAndWait()
             return "An exception has occurred!"
@@ -153,6 +156,26 @@ if __name__ == "__main__":
     # Runs the app
     app.window.mainloop()
 
+# engine = pyttsx3.init()
+# voices = engine.getProperty('voices')
+# engine.setProperty('voice', 'de')
+# engine.say("Dobar dan")
+# engine.runAndWait()
+
+# engine = pyttsx3.init()
+# german_voice_id = "com.apple.speech.synthesis.voice.anna"
+# engine.setProperty('voice', german_voice_id)
+
+# čitanje teksta s njemačkim glasom
+# engine.say("Guten Tag! Wie geht's Ihnen heute?")
+# engine.runAndWait()
+
+
+# engine = pyttsx3.init()
+# voices = engine.getProperty('voices')
+# engine.setProperty('voice', voices[1].id) 
+# engine.say("hello, i'm mary")
+# engine.runAndWait()
 
 # def ispis_odgovora_na_korisnikov_input():
 #         # #return(self.korisnikovo_pitanje.get())
@@ -188,3 +211,5 @@ if __name__ == "__main__":
 # # print(res)
 
 # #ispis_odgovora_na_korisnikov_input()
+
+#print(wikipedia.languages())
